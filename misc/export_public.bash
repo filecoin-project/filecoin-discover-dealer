adcefd812f932b210933752c3587014a87d999d2
Peter Rabbitson <ribasushi@protocol.ai>
2023-01-04 10:17:31 +0100

    Mothball original onboarder

--- misc/export_public.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-
-psql -At service=discover -c "
-  SELECT client FROM clients WHERE non_activated_datacap > 128 * 1024 * 1024 ORDER BY entry_id
-" \
-| "$( dirname "${BASH_SOURCE[0]}" )/atomic_cat.bash" "$HOME/WEB/public/clients.txt"
